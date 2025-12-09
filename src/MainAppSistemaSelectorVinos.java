f39c5d76accaa297ba3561aa8cbe61853f8a3f48
Jonathan Pacheco <JonathanPachecoCruz@hotmail.com>
2025-12-08 21:54:03 -0600

    feat: wineApp.onStart(); method

--- src/MainAppSistemaSelectorVinos.java
+++ src/MainAppSistemaSelectorVinos.java
@@ -1,5 +1,8 @@
-public class MainAppSistemaSelectorVinos {
-    public static void main(String[] args) {
+import net.trashlib.system.selector.wines.events.WineApp;
 
-    }
+public class MainAppSistemaSelectorVinos {
+  public static void main(String[] args) {
+    WineApp wineApp = new WineApp();
+    wineApp.onStart();
+  }
 }
