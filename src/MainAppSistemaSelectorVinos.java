aa2fd009f97dd1a0bf7a3f79e3b6252abb61637c
Jonathan Pacheco Cruz <JonathanPCruz.dev@hotmail.com>
2026-01-12 17:22:27 -0600

    chore: Se hace identacion con formateo

--- src/MainAppSistemaSelectorVinos.java
+++ src/MainAppSistemaSelectorVinos.java
@@ -1,8 +1,8 @@
 import net.trashlib.system.selector.wines.events.WineApp;
 
 public class MainAppSistemaSelectorVinos {
-  public static void main(String[] args) {
-    WineApp wineApp = new WineApp();
-    wineApp.onStart();
-  }
+    public static void main(String[] args) {
+        WineApp wineApp = new WineApp();
+        wineApp.onStart();
+    }
 }
