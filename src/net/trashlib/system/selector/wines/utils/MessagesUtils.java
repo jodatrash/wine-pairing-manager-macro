ccfb32633af6cc4b5167ea62a838dba87d08cfc9
Jonathan Pacheco Cruz <JonathanPCruz.dev@hotmail.com>
2026-01-12 17:23:31 -0600

    chore: Se hace identacion con formateo

--- src/net/trashlib/system/selector/wines/utils/MessagesUtils.java
+++ src/net/trashlib/system/selector/wines/utils/MessagesUtils.java
@@ -5,6 +5,7 @@ public class MessagesUtils {
     private String MESSAGE_INITIAL = "Elegimos el vino adecuado para tu comida.";
     private String QUESTION_MESSAGE = "Responde correctamente con: \n(y)es. \n(n)o.";
     private String ERROR_MESSAGE = "Por favor seleccione una opción valida!";
+    private String CONFIRM_MESSAGE = "Are you sure you want to continue connecting (yes/no)?";
 
     public String getWelcomeMessage() {
         return WELCOME_MESSAGE;
@@ -17,6 +18,7 @@ public class MessagesUtils {
     public String getQuestionMessage() {
         return QUESTION_MESSAGE;
     }
+
     public String getErrorMessage() {
         return ERROR_MESSAGE;
     }
--- src/net/trashlib/system/selector/wines/utils/WineList.java
+++ src/net/trashlib/system/selector/wines/utils/WineList.java
@@ -1,51 +1,51 @@
 package net.trashlib.system.selector.wines.utils;
 
 public class WineList {
-  private String[] redWineListAlcohol = {
-          "bordeaux",
-          "Beaujolais",
-          "Cabernet",
-          "Carmenere",
-          "Merlot",
-          "Pinot",
-          "Pinotage",
-          "Sangiovanese",
-          "Syrah",
-          "Tempranillo",
-          "Zinfandel"
-  };
+    private String[] redWineListAlcohol = {
+            "bordeaux",
+            "Beaujolais",
+            "Cabernet",
+            "Carmenere",
+            "Merlot",
+            "Pinot",
+            "Pinotage",
+            "Sangiovanese",
+            "Syrah",
+            "Tempranillo",
+            "Zinfandel"
+    };
 
-  private String[] whiteWineListAlcohol = {
-          "albarino",
-          "chablis",
-          "chardonay",
-          "pinot",
-          "riesling",
-          "sauvignon"
-  };
-  private String[] sparklingListAlcohol = {
-          "espumante-brut",
-          "espumante-demi-sec",
-          "espumante-rose",
-          "espumante-dulce",
-          "espumante-seco"
-  };
+    private String[] whiteWineListAlcohol = {
+            "albarino",
+            "chablis",
+            "chardonay",
+            "pinot",
+            "riesling",
+            "sauvignon"
+    };
+    private String[] sparklingListAlcohol = {
+            "espumante-brut",
+            "espumante-demi-sec",
+            "espumante-rose",
+            "espumante-dulce",
+            "espumante-seco"
+    };
 
-  public WineList(String[] redWineListAlcohol, String[] whiteWineListAlcohol, String[] sparklingListAlcohol) {
-    this.redWineListAlcohol = redWineListAlcohol;
-    this.whiteWineListAlcohol = whiteWineListAlcohol;
-    this.sparklingListAlcohol = sparklingListAlcohol;
-  }
+    public WineList(String[] redWineListAlcohol, String[] whiteWineListAlcohol, String[] sparklingListAlcohol) {
+        this.redWineListAlcohol = redWineListAlcohol;
+        this.whiteWineListAlcohol = whiteWineListAlcohol;
+        this.sparklingListAlcohol = sparklingListAlcohol;
+    }
 
-  public String[] getWhiteWineListAlcohol() {
-    return whiteWineListAlcohol;
-  }
+    public String[] getWhiteWineListAlcohol() {
+        return whiteWineListAlcohol;
+    }
 
-  public String[] getSparklingListAlcohol() {
-    return sparklingListAlcohol;
-  }
+    public String[] getSparklingListAlcohol() {
+        return sparklingListAlcohol;
+    }
 
-  public String[] getRedWineListAlcohol() {
-    return redWineListAlcohol;
-  }
+    public String[] getRedWineListAlcohol() {
+        return redWineListAlcohol;
+    }
 }
