5243354fe9d3b688ac2410135127054701121255
Jonathan Pacheco Cruz <JonathanPCruz.dev@hotmail.com>
2026-02-13 20:53:24 -0600

    feat: add new package and create ApplicationConstant class

---
+++ src/com/wine/pairing/macro/constants/ApplicationConstants.java
@@ -0,0 +1,8 @@
+package com.wine.pairing.macro.constants;
+
+public class ApplicationConstants {
+    public static final int MIN_RANGE = 0;
+    public static final int MAX_RANGE = 3;
+    public static final int ERROR_CODE = -1;
+    public static final int EXIT_OPTION = 0;
+}
