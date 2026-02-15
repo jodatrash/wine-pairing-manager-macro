8ac467c5da05ea2209892683882e69e29f832d60
Jonathan Pacheco Cruz <JonathanPCruz.dev@hotmail.com>
2026-02-14 23:23:46 -0600

    refactor: add attributes to Wine class

--- src/com/wine/pairing/macro/models/Wine.java
+++ src/com/wine/pairing/macro/models/Wine.java
@@ -1,4 +1,18 @@
 package com.wine.pairing.macro.models;
 
 public class Wine {
+    private static String name;
+    private static String type;
+    private static boolean noAlcohol;
+    private static boolean withAlcohol;
+
+    public Wine() {
+        this.name = name;
+        this.type = type;
+        this.noAlcohol = noAlcohol;
+        this.withAlcohol = withAlcohol;
+
+    }
+
+
 }
